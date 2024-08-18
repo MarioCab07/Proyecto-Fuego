f757466a72368be96c194251f9c678e615505b4f
MarioCab07 <00002022@uca.edu.sv>
2024-08-17 22:40:09 -0600

    connection to mongo completed

--- proyect-service/app.js
+++ proyect-service/app.js
@@ -2,15 +2,18 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
+const dabatase = require('./config/database.config');
 
 
 let app = express();
-
+dabatase.connect();
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
